ab737073448a6f35316916181f03bf00f3b9fcd5
Music-euro <108573536+Music-euro@users.noreply.github.com>
2022-07-07 23:24:01 +0200

    Add files via upload

--- JE313P/plugins/ping.py
+++ JE313P/plugins/ping.py
@@ -5,7 +5,7 @@ from telethon import Button, events
 from JE313P import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/d49949a966c8219f4fa8c.jpg"
+    "PING_PIC", "https://telegra.ph/file/727e6b2141a3656ef0262.jpg"
 )
 ms = 4
 
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنج:** {ms}\n المالك:『{ALIVE}』"
 
 @JE313P.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    UMM = [[Button.url("السورس", "https://t.me/E_U_R_O_1")]]
+    UMM = [[Button.url("السورس", "https://t.me/Source_Joo")]]
     await JE313P.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
